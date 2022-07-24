5c37f6c1f859f9fe7905209b9bdb9df3de5f3c4b
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-07-24 09:47:31 +0800

    Use the generated verison of release.ex

--- apps/longlink/lib/longlink/release.ex
+++ apps/longlink/lib/longlink/release.ex
@@ -1,18 +1,28 @@
-defmodule Longlink.Release do
-  @app :longlink
+defmodule LonglinkWeb.Release do
+  @moduledoc """
+  Used for executing DB release tasks when run in production without Mix
+  installed.
+  """
+  @app :longlink_web
 
-  def migrate() do
+  def migrate do
+    load_app()
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
+    load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
-  defp repos() do
-    Application.load(@app)
+  defp repos do
     Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
   end
 end
